abe4b5f69c6ee478d8f7e7f81743aad5a68d12e4
neeravbhaskarla <ananthaneerav@gmail.com>
2025-09-21 23:44:07 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NeuralBackground />
+    <main style="position: relative; z-index: 2; color: white; text-align: center; padding: 3rem;">
+      <h1>Hello Vue + Neural Background</h1>
+      <p>This should float above the animated canvas.</p>
+    </main>
+  </div>
+</template>
+
+<script>
+import NeuralBackground from "./components/NeuralBackground.vue";
+
+export default {
+  components: { NeuralBackground },
+};
+</script>
